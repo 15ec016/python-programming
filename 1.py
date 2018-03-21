50282500a874afae7446d1e91ff196b1554a733e
15ec016 <harikrish9798@gmail.com>
2018-03-21 16:24:47 +0530

    Create 1.py

---
+++ 1.py
@@ -0,0 +1,19 @@
+def timetomin(t0,t1):
+	i1=t0.split(':')
+	i2=t1.split(':')
+	(tt0,tt1)=(int(i1[0]),int(i2[0]))
+	(tm0,tm1)=(int(i1[1]),int(i2[1]))
+	if tt0<tt1:
+		return False
+	time=tt0-tt1
+	mins=tm0-tm1
+	mins+=(time*60)
+	print('Minutes :',mins)
+def main():
+	try:
+		t0=input()
+		t1=input()
+		timetomin(t0,t1)
+	except:
+		print('invalid')
+main()
